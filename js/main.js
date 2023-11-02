83492d494902f5744e15bcc30b119046dce0e294
jparrker <102701917+jparrker@users.noreply.github.com>
2023-11-02 19:08:13 -0400

    added js functionality for next prev buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-// Start writing JavaScript here!
+const carousel = document.querySelector(".carousel");
+const previousButton = carousel.querySelector(".previous-button");
+const nextButton = carousel.querySelector(".next-button");
+const contents = carousel.querySelector(".carousel__contents");
+
+nextButton.addEventListener("click", (event) => {
+  const currentSlide = contents.querySelector(".is-selected");
+  const nextSlide = currentSlide.nextElementSibling;
+  const destination = getComputedStyle(nextSlide).left;
+  contents.style.left = "-" + destination;
+  currentSlide.classList.remove("is-selected");
+  nextSlide.classList.add("is-selected");
+  previousButton.removeAttribute("hidden");
+  if (!nextSlide.nextElementSibling) {
+    nextButton.setAttribute("hidden", true);
+  }
+});
+
+previousButton.addEventListener("click", (event) => {
+  const currentSlide = contents.querySelector(".is-selected");
+  const previousSlide = currentSlide.previousElementSibling;
+  const destination = getComputedStyle(previousSlide).left;
+  contents.style.left = "-" + destination;
+  currentSlide.classList.remove("is-selected");
+  previousSlide.classList.add("is-selected");
+  nextButton.removeAttribute("hidden");
+  if (!previousSlide.previousElementSibling) {
+    previousButton.setAttribute("hidden", true);
+  }
+});
